190b0c115590ca8390271847aa2771132245171c
Dheeraj Chahar <d.chahar@tcs.com>
2021-05-17 20:23:13 +0530

    Integrated Metal with AR scene kit

---
+++ AR+Metal/ARPlaneAnchor+Extension.swift
@@ -0,0 +1,63 @@
+//
+//  ARPlaneAnchor+Extension.swift
+//  AR+Metal
+//
+//  Created by Dheeraj Chahar on 17/05/21.
+//
+
+import ARKit
+
+extension ARPlaneAnchor {
+    
+    @discardableResult
+    func addPlaneNode(on node: SCNNode, geometry: SCNGeometry, contents: Any) -> SCNNode {
+        guard let material = geometry.materials.first else { fatalError() }
+        
+        if let program = contents as? SCNProgram {
+            material.program = program
+        } else {
+            material.diffuse.contents = contents
+        }
+        
+        let planeNode = SCNNode(geometry: geometry)
+        
+        DispatchQueue.main.async(execute: {
+            node.addChildNode(planeNode)
+        })
+        
+        return planeNode
+    }
+
+    func addPlaneNode(on node: SCNNode, contents: Any) {
+        let geometry = SCNPlane(width: CGFloat(extent.x), height: CGFloat(extent.z))
+        let planeNode = addPlaneNode(on: node, geometry: geometry, contents: contents)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1, 0, 0)
+    }
+    
+    func findPlaneNode(on node: SCNNode) -> SCNNode? {
+        for childNode in node.childNodes {
+            if childNode.geometry as? SCNPlane != nil {
+                return childNode
+            }
+        }
+        return nil
+    }
+
+    func updatePlaneNode(on node: SCNNode) {
+        DispatchQueue.main.async(execute: {
+            guard let plane = self.findPlaneNode(on: node)?.geometry as? SCNPlane else { return }
+            guard !PlaneSizeEqualToExtent(plane: plane, extent: self.extent) else { return }
+            
+            plane.width = CGFloat(self.extent.x)
+            plane.height = CGFloat(self.extent.z)
+        })
+    }
+}
+
+fileprivate func PlaneSizeEqualToExtent(plane: SCNPlane, extent: vector_float3) -> Bool {
+    if plane.width != CGFloat(extent.x) || plane.height != CGFloat(extent.z) {
+        return false
+    } else {
+        return true
+    }
+}
---
+++ AR+Metal/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  AR+Metal
+//
+//  Created by Dheeraj Chahar on 17/05/21.
+//
+
+import UIKit
+import MetalKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.sceneView.delegate = self
+        self.sceneView.scene = SCNScene()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.sceneView.session.pause()
+    }
+}
+    
+// MARK: - ARSCNViewDelegate
+
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let currentFrame = self.sceneView.session.currentFrame else { return }
+        
+        for anchor in currentFrame.anchors {
+            guard let planeAnchor = anchor as? ARPlaneAnchor else { continue }
+            guard let node = self.sceneView.node(for: planeAnchor) else { continue }
+            
+            DispatchQueue.main.async {
+                let planeNode = planeAnchor.findPlaneNode(on: node)
+                guard let material = planeNode?.geometry?.firstMaterial else { return }
+                self.updateTime(time, for: material)
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { fatalError() }
+        
+        // Shaders for the material
+        let program = SCNProgram()
+        program.vertexFunctionName = "scnVertexShader"
+        program.fragmentFunctionName = "scnFragmentShader"
+
+        planeAnchor.addPlaneNode(on: node, contents: program)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { fatalError() }
+        planeAnchor.updatePlaneNode(on: node)
+    }
+    
+    private func updateTime(_ time: TimeInterval, for material: SCNMaterial) {
+        var floatTime = Float(time)
+        let timeData = Data(bytes: &floatTime, count: MemoryLayout<Float>.size)
+        material.setValue(timeData, forKey: "time")
+    }
+}
